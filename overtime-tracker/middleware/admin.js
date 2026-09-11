2f6489d02ff5f3a5444673ed307b0b62ff217c11
Caio Marinho <78876211+MarinhoLGTM@users.noreply.github.com>
2026-09-11 14:26:06 +0100

    Delete overtime-tracker/middleware directory

--- overtime-tracker/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-function exigirAdmin(req, res, next) {
-  if (!req.usuario || req.usuario.papel !== 'admin') {
-    return res.status(403).json({ erro: 'Acesso restrito a administradores.' });
-  }
-  next();
-}
-
-module.exports = exigirAdmin;
